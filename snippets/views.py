723d63fac5f89cee4e09bd867cd5effe99467157
har07 <dev.har07@gmail.com>
2016-02-13 21:18:23 +0700

    tutorial part 4. Bugfix prev commit,  restrict update/delete permission to owner only

--- snippets/views.py
+++ snippets/views.py
@@ -12,6 +12,7 @@ from rest_framework import permissions
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 from django.contrib.auth.models import User
 
@@ -30,7 +31,8 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
